3f9e515c8d43a87ba0505684d3d12f69806ba4f3
Daniel Molina <daniel_molina@epam.com>
2025-12-03 07:15:29 -0500

    reverseWords complete

--- app/strings_validator.js
+++ app/strings_validator.js
@@ -11,6 +11,26 @@ class StringsValidator {
     }
     return word[0].toUpperCase() + word.slice(1).toLowerCase();
   }
+
+  reverseWords(word) {
+    if (word == null) {
+      throw new Error("Input must not be null");
+    }
+    if (typeof word !== "string") {
+      throw new Error("Input must be a string");
+    }
+    if (word.trim().length < 1) {
+      throw new Error("Input must not be empty");
+    }
+    if (word.length < 1) {
+      throw new Error("Input cannot be empty");
+    }
+    let splitWord = word.trim().split(" ");
+    let removeSpaces = splitWord.filter((space) => Boolean(space));
+    removeSpaces = removeSpaces.reverse();
+    let joinWord = removeSpaces.join(" ");
+    return joinWord;
+  }
 }
 
 module.exports = StringsValidator;
--- test/strings_validator/stringsValidator.spec.js
+++ test/strings_validator/stringsValidator.spec.js
@@ -31,4 +31,56 @@ describe("StringsValidator", () => {
       expect(validator.capitalize("adrian")).to.equal("Adrian");
     });
   });
+
+  describe("reverseWords", () => {
+    it("should throw error if null", () => {
+      expect(() => validator.reverseWords(null)).to.throw(
+        "Input must not be null"
+      );
+    });
+
+    it("should throw error if not a string", () => {
+      expect(() => validator.reverseWords(50)).to.throw(
+        "Input must be a string"
+      );
+    });
+
+    it("should throw error if empty", () => {
+      expect(() => validator.reverseWords("")).to.throw(
+        "Input cannot be empty"
+      );
+    });
+
+    it("it should throw error if only spaces", () => {
+      expect(() => validator.reverseWords("     ")).to.throw(
+        "Input must not be empty"
+      );
+    });
+
+    it("should be the same word with no changes", () => {
+      expect(validator.reverseWords("Hola")).to.equal("Hola");
+    });
+
+    it("should reverse sentence", () => {
+      expect(validator.reverseWords("Hola papa")).to.equal("papa Hola");
+    });
+
+    it("should reverse sentence with special characters", () => {
+      expect(validator.reverseWords("C@arlos, Sergio's and myself.")).to.equal(
+        "myself. and Sergio's C@arlos,"
+      );
+    });
+
+    it("should remove extra spaces in between and reverse sentence", () => {
+      expect(validator.reverseWords("Hola   papa")).to.equal("papa Hola");
+    });
+
+    it("should remove leading/trailing spaces and reverse sentence", () => {
+      expect(validator.reverseWords("   Hola papa   ")).to.equal("papa Hola");
+    });
+
+    it("should remove multiple spaces in between and leading/trailing spaces and reverse sentence", () => {
+      expect(validator.reverseWords("   Hola   papa   ")).to.equal("papa Hola");
+    });
+  });
 });
